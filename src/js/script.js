8b634ae93bcf7fca62c29a808fcf013ca67cdb25
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-08-03 17:32:51 -0300

    adicionar novas alterações

--- src/js/script.js
+++ src/js/script.js
@@ -3,19 +3,29 @@ const audio = document.getElementById('audio');
 const botaoPlay = document.getElementById('play');
 const botaoVoltar = document.getElementById('voltar');
 const botaoAvancar = document.getElementById('avancar');
-const imagem = document.getElementById('img');
 const titulodaMusica = document.getElementById('titulo');
 const nomeBanda = document.getElementById('banda');
+const imagem = document.getElementById('img');
 const icone = document.getElementById('icone');
 let taTocando = false;
 let musicaAtual = 1;
 let quantidade = 2;
-let imagemAtual = 1;
 const corpo = document.body;
 const musicas = [
     { banda: 'Cosmo Sheldrake', musica: 'Lost in the City Lights', cor: '#47284A' },
     { banda: 'Lesfim', musica: 'Forest lullaby', cor: '#1F011B' }
 ];
+function atualizarInterface() {
+    if (titulodaMusica)
+        titulodaMusica.innerHTML = musicas[musicaAtual - 1].musica;
+    if (nomeBanda)
+        nomeBanda.innerHTML = musicas[musicaAtual - 1].banda;
+    corpo.style.background = musicas[musicaAtual - 1].cor;
+    if (audio)
+        audio.src = `src/audio/${musicaAtual}.mp3`;
+    if (imagem)
+        imagem.src = `src/images/${musicaAtual}.png`;
+}
 function tocarMusica() {
     audio === null || audio === void 0 ? void 0 : audio.play();
     icone === null || icone === void 0 ? void 0 : icone.classList.add('bi-pause-fill');
@@ -27,63 +37,17 @@ function pausarMusica() {
     icone === null || icone === void 0 ? void 0 : icone.classList.remove('bi-pause-fill');
 }
 function tocarePausarMusica() {
-    if (taTocando === false) {
-        taTocando = true;
-        tocarMusica();
-    }
-    else {
-        taTocando = false;
-        pausarMusica();
-    }
+    taTocando ? pausarMusica() : tocarMusica();
+    taTocando = !taTocando;
 }
 function proximaMusica() {
-    if (musicaAtual === quantidade) {
-        musicaAtual = 1;
-        imagemAtual = 1;
-        if (titulodaMusica)
-            titulodaMusica.innerHTML = musicas[0].musica;
-        if (nomeBanda)
-            nomeBanda.innerHTML = musicas[0].banda;
-        corpo.style.background = musicas[0].cor;
-    }
-    else {
-        musicaAtual += 1;
-        imagemAtual += 1;
-        if (titulodaMusica)
-            titulodaMusica.innerHTML = musicas[1].musica;
-        if (nomeBanda)
-            nomeBanda.innerHTML = musicas[1].banda;
-        corpo.style.background = musicas[1].cor;
-    }
-    if (audio)
-        audio.src = 'src/audio/' + musicaAtual + '.mp3';
-    if (imagem)
-        imagem.src = 'src/imagens/' + imagemAtual + '.png';
+    musicaAtual = musicaAtual === quantidade ? 1 : musicaAtual + 1;
+    atualizarInterface();
     tocarMusica();
 }
 function voltarMusica() {
-    if (musicaAtual === 1) {
-        musicaAtual = quantidade;
-        imagemAtual = quantidade;
-        if (titulodaMusica)
-            titulodaMusica.innerHTML = musicas[1].musica;
-        if (nomeBanda)
-            nomeBanda.innerHTML = musicas[1].banda;
-        corpo.style.background = '#1F011B';
-    }
-    else {
-        musicaAtual -= 1;
-        imagemAtual -= 1;
-        if (titulodaMusica)
-            titulodaMusica.innerHTML = musicas[0].musica;
-        if (nomeBanda)
-            nomeBanda.innerHTML = musicas[0].banda;
-        corpo.style.background = musicas[0].cor;
-    }
-    if (audio)
-        audio.src = `src/audio/${musicaAtual}.mp3`;
-    if (imagem)
-        imagem.src = `src/imagens/${imagemAtual}.png`;
+    musicaAtual = musicaAtual === 1 ? quantidade : musicaAtual - 1;
+    atualizarInterface();
     tocarMusica();
 }
 botaoPlay === null || botaoPlay === void 0 ? void 0 : botaoPlay.addEventListener('click', () => tocarePausarMusica());
--- src/typescript/script.ts
+++ src/typescript/script.ts
@@ -1,22 +1,30 @@
-const audio: HTMLElement | null = document.getElementById('audio')
-const botaoPlay: HTMLButtonElement | null = document.getElementById('play') as HTMLButtonElement | null
-const botaoVoltar: HTMLButtonElement | null = document.getElementById('voltar') as HTMLButtonElement | null
-const botaoAvancar: HTMLButtonElement | null = document.getElementById('avancar') as HTMLButtonElement | null
-const imagem: HTMLElement | null = document.getElementById('img')
-const titulodaMusica: HTMLElement | null = document.getElementById('titulo')
-const nomeBanda: HTMLElement | null = document.getElementById('banda')
-const icone: HTMLElement | null = document.getElementById('icone')
+const audio= document.getElementById('audio') as HTMLElement | null
+const botaoPlay = document.getElementById('play') as HTMLButtonElement | null
+const botaoVoltar = document.getElementById('voltar') as HTMLButtonElement | null
+const botaoAvancar = document.getElementById('avancar') as HTMLButtonElement | null
+const titulodaMusica = document.getElementById('titulo') as HTMLElement | null
+const nomeBanda= document.getElementById('banda') as HTMLElement | null
+const imagem = document.getElementById('img')
+const icone= document.getElementById('icone') as HTMLElement | null
 
 let taTocando: boolean = false
 let musicaAtual: number = 1
 let quantidade: number = 2
-let imagemAtual: number = 1
 const corpo = document.body
 
-const musicas= [
+// Criei um objeto para colocar os nomes das muisca bandas e cores quando eu avançar uma música e voltar
+const musicas = [
     {banda: 'Cosmo Sheldrake', musica: 'Lost in the City Lights', cor: '#47284A'},
     {banda: 'Lesfim', musica: 'Forest lullaby', cor: '#1F011B'}
 ]
+
+function atualizarInterface(): void {
+    if (titulodaMusica) titulodaMusica.innerHTML = musicas[musicaAtual - 1].musica // Esse if verifica se ele é nulo primeiro
+    if (nomeBanda) nomeBanda.innerHTML = musicas[musicaAtual - 1].banda
+    corpo.style.background = musicas[musicaAtual - 1].cor
+    if (audio) audio.src = `src/audio/${musicaAtual}.mp3` // Já coloca direto o caminho do audio nessa função
+    if (imagem) imagem.src = `src/images/${musicaAtual}.png` // Já coloca direto o caminho da imagem nessa função
+}
 
 function tocarMusica(): void {
     audio?.play()
@@ -31,53 +39,19 @@ function pausarMusica(): void {
 }
 
 function tocarePausarMusica(): void {
-    if (taTocando === false) {
-        taTocando = true
-        tocarMusica()
-    } else {
-        taTocando = false
-        pausarMusica()
-    }
+    taTocando ? pausarMusica() : tocarMusica() // verifica se ele é verdadeiro se for pausa musica se não toca musica, com condição ternaria.
+    taTocando = !taTocando // Deixa ele falso novamente.
 }
 
 function proximaMusica(): void {
-    if (musicaAtual === quantidade) {
-        musicaAtual = 1
-        imagemAtual = 1
-        if (titulodaMusica) titulodaMusica.innerHTML = musicas[0].musica
-        if (nomeBanda) nomeBanda.innerHTML = musicas[0].banda
-        corpo.style.background = musicas[0].cor
-
-    } else {
-        musicaAtual += 1
-        imagemAtual += 1
-        if (titulodaMusica) titulodaMusica.innerHTML = musicas[1].musica
-        if (nomeBanda) nomeBanda.innerHTML = musicas[1].banda
-        corpo.style.background = musicas[1].cor
-    }
-
-    if (audio) audio.src = 'src/audio/' + musicaAtual + '.mp3'
-    if (imagem) imagem.src = 'src/imagens/' + imagemAtual + '.png'
+    musicaAtual = musicaAtual === quantidade ? 1 : musicaAtual + 1 
+    atualizarInterface()
     tocarMusica()
 }
 
 function voltarMusica(): void {
-    if (musicaAtual === 1) {
-        musicaAtual = quantidade
-        imagemAtual = quantidade
-        if (titulodaMusica) titulodaMusica.innerHTML = musicas[1].musica
-        if (nomeBanda) nomeBanda.innerHTML = musicas[1].banda
-        corpo.style.background = '#1F011B'
-    } else {
-        musicaAtual -= 1
-        imagemAtual -= 1
-        if (titulodaMusica) titulodaMusica.innerHTML = musicas[0].musica
-        if (nomeBanda) nomeBanda.innerHTML = musicas[0].banda
-        corpo.style.background = musicas[0].cor
-    }
-
-    if (audio) audio.src = `src/audio/${musicaAtual}.mp3`
-    if (imagem) imagem.src = `src/imagens/${imagemAtual}.png`
+    musicaAtual = musicaAtual === 1 ? quantidade : musicaAtual - 1
+    atualizarInterface()
     tocarMusica()
 }
 
